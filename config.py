f110a9d678395c7ca1da3b18f6080ee31c650aaf
GOKU <dr084891@gmail.com>
2024-07-28 03:36:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
+API_ID = int(getenv("API_ID", "21415583")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID","6762113050"))
+OWNER_ID = int(getenv("OWNER_ID","6462365139"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://graph.org/file/c6a2ed96648fd03377dc9.jpg')
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://graph.org/file/eabfb1087a5508dbbb218.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/itzshukla/STRANGER-USERBOT2.0")
+REPO_URL = getenv("REPO_URL", "https://github.com/Mrdadgoku/GOKU-USERBOT2.0")
 BRANCH = getenv("BRANCH", "master") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
